83ec50aed7a9645fc530d722a7282cc4e0b879b6
Filipe Paisana <20220843@iade.pt>
2023-12-07 01:28:08 +0000

    Menus acabados e a passarem para ativitys

--- app/src/main/java/pt/iade/ptiadefilipepaisanaatividade/MainActivity.java
+++ app/src/main/java/pt/iade/ptiadefilipepaisanaatividade/MainActivity.java
@@ -1,57 +1,47 @@
 package pt.iade.ptiadefilipepaisanaatividade;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private List<NoteItem> notes = new ArrayList<>();
-    private NoteAdapter notesAdapter;
-    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        recyclerView = findViewById(R.id.todo_list);
+        setupComponents();
     }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onPrepareOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_new_note) {
-            createNewNote();
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_add_note) {
+            Intent intent = new Intent(MainActivity.this, NoteActivity.class);
+            startActivity(intent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-    private void addNewNote(String title, String summary, String content, String creationDate, String modifiedDate) {
-        notes.add(new NoteItem(title, summary, "2020-10-10", "2020-10-10"));
-        notesAdapter.notifyItemInserted(notes.size() - 1);
-    }
-    private void createNewNote() {
 
-        addNewNote(
-                "Note " + (notes.size() + 1),
-                "Summary " + (notes.size() + 1),
-                "Content " + (notes.size() + 1),
-                "2020-10-10",
-                "2020-10-10");
+    private void setupComponents() {
+        setSupportActionBar(findViewById(R.id.toolbar));
+
     }
 }

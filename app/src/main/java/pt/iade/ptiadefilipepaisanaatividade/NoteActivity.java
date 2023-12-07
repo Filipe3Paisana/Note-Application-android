7d6a25c0d90424a420fa2b9f870a6ce10bc15f4e
Filipe Paisana <20220843@iade.pt>
2023-12-07 00:18:56 +0000

    Created the basic layout

--- app/src/main/java/pt/iade/ptiadefilipepaisanaatividade/NoteActivity.java
+++ app/src/main/java/pt/iade/ptiadefilipepaisanaatividade/NoteActivity.java
@@ -1,30 +1,14 @@
 package pt.iade.ptiadefilipepaisanaatividade;
 
-import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import android.os.Bundle;
 
 public class NoteActivity extends AppCompatActivity {
-    private EditText titleEditText;
-    private EditText contentEditText;
-    private TextView modifiedDateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        titleEditText = findViewById(R.id.titleEditText);
-        contentEditText = findViewById(R.id.contentEditText);
-        modifiedDateTextView = findViewById(R.id.modifiedDateTextView);
-
-        // Aqui você pode inicializar os campos com dados, se estiver editando uma nota existente
     }
-
-    // Métodos para salvar e excluir a nota aqui
 }

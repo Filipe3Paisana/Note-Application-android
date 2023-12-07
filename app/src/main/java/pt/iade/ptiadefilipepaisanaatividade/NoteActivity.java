a6aa65557fd9552d1457af72aa9e10900968f749
Filipe Paisana <20220843@iade.pt>
2023-12-07 02:29:03 +0000

    Crear o model e mapeamento na activity

--- app/src/main/java/pt/iade/ptiadefilipepaisanaatividade/NoteActivity.java
+++ app/src/main/java/pt/iade/ptiadefilipepaisanaatividade/NoteActivity.java
@@ -1,14 +1,93 @@
 package pt.iade.ptiadefilipepaisanaatividade;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import android.view.MenuItem;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.GregorianCalendar;
+
+import pt.iade.ptiadefilipepaisanaatividade.models.NoteItem;
 
 public class NoteActivity extends AppCompatActivity {
+    protected EditText noteTitle;
+    protected EditText noteBody;
+    protected TextView lastModified;
 
+    protected NoteItem item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        item= new NoteItem(1,"Teste","Teste",new GregorianCalendar(),new GregorianCalendar());
+
+
+        setupComponents();
     }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_note, menu);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.save_note) {
+            finish();
+            return true;
+        }
+
+        if(item.getItemId() == R.id.delete_note) {
+            showDeleteConfirmationDialog();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupComponents() {
+        setSupportActionBar(findViewById(R.id.toolbar));
+        noteTitle =  findViewById(R.id.note_title);
+        noteBody = findViewById(R.id.note_body);
+        lastModified = findViewById(R.id.last_modified);
+        populateView();
+    }
+    protected void populateView() {
+        noteTitle.setText(item.getTitle());
+        noteBody.setText(item.getContent());
+        lastModified.setText(item.getModifiedDateAsString().toString());
+    }
+    private void showDeleteConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmar");
+        builder.setMessage("Tem certeza que deseja deletar esta nota?");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
 }
---
+++ app/src/main/java/pt/iade/ptiadefilipepaisanaatividade/models/NoteItem.java
@@ -0,0 +1,76 @@
+package pt.iade.ptiadefilipepaisanaatividade.models;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class NoteItem {
+   int id;
+
+    String title;
+    String summary;
+    String content;
+    Calendar creationDate;
+    Calendar modifiedDate;
+
+    public NoteItem(){
+        this(0,"","",new  GregorianCalendar(),new GregorianCalendar());
+    }
+
+    public NoteItem(int id,String title, String content, Calendar creationDate, Calendar modifiedDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.creationDate = creationDate;
+        this.modifiedDate = modifiedDate;
+        this.summary = content.substring(0, Math.min(content.length(), 80));
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public Calendar getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Calendar creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Calendar getModifiedDate() {
+        return modifiedDate;
+    }
+    public String getModifiedDateAsString() {
+        return modifiedDate.get(Calendar.DAY_OF_MONTH) + "/" +
+                modifiedDate.get(Calendar.MONTH) + "/" +
+                modifiedDate.get(Calendar.YEAR) + " " +
+                modifiedDate.get(Calendar.HOUR_OF_DAY) + ":" +
+                modifiedDate.get(Calendar.MINUTE);
+    }
+    public void setModifiedDate(Calendar modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
